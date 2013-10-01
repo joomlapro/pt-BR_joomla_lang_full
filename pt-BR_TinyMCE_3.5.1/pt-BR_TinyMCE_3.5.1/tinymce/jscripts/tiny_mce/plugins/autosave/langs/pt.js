48f6a288dcc6c887fca40797dac6f65287d90bad
Bruno Batista <bruno@brunobatista.eti.br>
2013-10-01 15:36:07 -0300

    Remade the structure of the files

---
+++ pt-BR_TinyMCE_3.5.1/pt-BR_TinyMCE_3.5.1/tinymce/jscripts/tiny_mce/plugins/autosave/langs/pt.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('pt.autosave',{
+restore_content: "Restaurar conteúdo salvo automáticament",
+warning_message: ""Se você restaurar o conteúdo gravado, você vai perder todo o conteúdo que está atualmente no editor.\n\nVocê tem certeza de que deseja restaurar o conteúdo salvo?"
+});
