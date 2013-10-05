06d6afefeb34c043bc9c3c7ea6082e5416078f77
Bruno Batista <bruno@brunobatista.eti.br>
2013-10-05 10:34:26 -0300

    Removed plugins language of TinyMCE

--- TinyMCE_pt-BR/tinymce/plugins/autosave/langs/pt-BR.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addI18n('pt.autosave',{
-restore_content: "Restaurar conteúdo salvo automáticament",
-warning_message: ""Se você restaurar o conteúdo gravado, você vai perder todo o conteúdo que está atualmente no editor.\n\nVocê tem certeza de que deseja restaurar o conteúdo salvo?"
-});
